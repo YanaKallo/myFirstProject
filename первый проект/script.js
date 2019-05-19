722451d8f334dee287cd4d47b0bd97206dcf6641
Yana <manhuntkommt@gmail.com>
2019-05-19 19:25:34 +1000

    Это наш первый коммит

---
+++ первый проект/script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log(second);
+let person = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name);
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+//alert('Hi World!');
+
+//let answer = confirm('Are you here?');
+//console.log(answer)
+
+//let answer = +prompt('Есть ли Вам 18?', 'Да');
+//console.log(typeof(answer));
+
+//console.log('arr' + ' - object');
+//console.log(4 + ' - object');
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log('2' == 2);
+
+let isChecked = true,
+    isClosed = false;
+console.log(isChecked || !isClosed);
+//&&- оператор и, выполняется, когда оба true
+//|| - оператор или, выполняется, когда хоть один true
+// ! - знак минус превращает значение в противоположное
